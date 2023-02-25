97f21bb1ec94dc6a0f6571d48d06c9f5008bc4fd
Nice <nicenicegame@gmail.com>
2023-02-25 14:06:26 +0700

    add files

---
+++ fixtures/products/products.js
@@ -0,0 +1,29 @@
+const mongodb = require("mongodb");
+
+module.exports = [
+    {
+        _id: mongodb.ObjectId("63f9b27a10ad3e043dd2cd93"),
+        name: "Netflix",
+        url: 'https://www.netflix.com'
+    },
+    {
+        _id: mongodb.ObjectId("63f9b27d0dcd37267c329f1e"),
+        name: "Agoda",
+        url: 'https://www.agoda.com'
+    },
+    {
+        _id: mongodb.ObjectId("63f9b26b03104595c659b40d"),
+        name: "Youtube",
+        url: 'https://www.youtube.com'
+    },
+    {
+        _id: mongodb.ObjectId("63f9b270cd9b69714b91debf"),
+        name: "Amazon",
+        url: 'https://www.amazon.com'
+    },
+    {
+        _id: mongodb.ObjectId("63f9b2745ee316308a7645f9"),
+        name: "Shopee",
+        url: 'https://www.netflix.co.th'
+    }
+];
